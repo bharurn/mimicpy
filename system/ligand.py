314ca957aa6202aeebbb8969a930d469365fd106
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2020-04-14 18:56:59 +0200

    Added match pdb2itp()

--- system/ligand.py
+++ system/ligand.py
@@ -11,6 +11,47 @@ class Ligand:
         self.chains = chains
         self.name = name
     
+    def _matchpdb2itp(self):
+        start = False
+        vals = {}
+        i = 1
+        
+        print("**Matching atom order in pdb to itp**")
+
+        print("Reading itp atoms..")
+        for line in self.itp.splitlines(): 
+            if "[ atoms ]" in line:
+                start = True
+            elif start:
+                splt = line.split()
+                if len(splt) == 0:
+                    break
+                if splt[0].isdigit():
+                    vals[splt[4]] = i
+                    i = i + 1
+        
+        pdb_str = ""
+        natms = 0
+        
+        print("Renumbering pdb atoms..")
+        for line in self.pdb.splitlines():
+            splt = hpdb.readLine(line)
+            if splt['record'] == "HETATM" or splt['record'] == "ATOM":
+                no = vals[splt['name']]
+                
+                if no > natms and splt['chainID'] == 'A': natms = no
+        
+                no_str = str( no + natms*( ord(splt['chainID'])-ord('A') ) )
+        
+                line = hpdb.editLine(line, serial=no_str)
+        
+            pdb_str += line+'\n'
+        
+        self.pdb = cmd.runinSeq(['sed', "s/ATOM  /HETATM/g"],
+                                ['print', "Sorting pdb atoms.."],
+                                ['sort', '-nk2'], stdin=pdb_str, decode=True)
+        print("**Done**")
+        
     @classmethod    
     def loadFromDF(cls, mol, pH=7, prep2pdb={}, tleap_dump=False):
         mol_name = mol.loc[0]['ChemCompId']
