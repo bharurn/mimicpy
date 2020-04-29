5cf209cca3fb20935f660ae7722fe1b5a0c013cf
bharurn <bharath.rn@gmail.com>
2020-04-29 20:40:06 +0200

    Eliminated rdkit dependency

--- system/ligand.py
+++ system/ligand.py
@@ -1,9 +1,9 @@
 from . import _addH, _getItp
 from ..utils import handlePDB as hpdb
-from .. import host as shell
-from rdkit.Chem import PandasTools
+from ..utils._misc import f
+from .._global import host as shell, gauss
 
-class Ligand:
+class NonStdLigand: 
     
     def __init__(self, pdb, itp, posre, chains, name, elems):
         self.pdb = pdb
@@ -11,7 +11,7 @@ class Ligand:
         self.posre = posre
         self.chains = chains
         self.name = name
-        self.elems = elems
+        self.elems = elems # for MiMiC
     
     def splitItp(self):
         start = False
@@ -37,7 +37,7 @@ class Ligand:
         vals = {}
         i = 1
         
-        print("**Matching atom order in pdb to itp**")
+        print("Matching atom order in pdb to itp..")
 
         print("Reading itp atoms..")
         for line in self.itp.splitlines(): 
@@ -71,15 +71,12 @@ class Ligand:
         self.pdb = pdb_str.replace('ATOM  ','HETATM')
         print("Sorting pdb atoms..")
         self.pdb = shell.cmd.run('sort -nk2', stdin=self.pdb)
-        print("**Done**")
     
     @classmethod
-    def _load(cls, mol, pH, mol_name, prep2pdb, tleap_dump):
-        pdb, chains = _addH.do(mol, pH, mol_name)
+    def _load(cls, mol, pH, prep2pdb, tleap_dump):
+        pdb, mol_name, chains, elems = _addH.do(mol, pH)
         
         itp, posre = _getItp.do(mol_name, prep2pdb, tleap_dump)
-        
-        elems = [a.GetSymbol() for a in mol.loc[0]['ROMol'].GetAtoms()] # for MiMiC
         
         lig = cls(pdb, itp, posre, chains, mol_name, elems)
         
@@ -89,76 +86,71 @@ class Ligand:
     
     @classmethod    
     def loadFromDF(cls, mol, pH=7, prep2pdb={}, tleap_dump=False):
-        mol_name = mol.loc[0]['ChemCompId']
+        print(f"Creating non standard ligand from SDF data..")
         
-        print(f"**Creating ligand {mol_name} from SDF Dataframe**")
-        
-        return cls._load(mol, pH, mol_name, prep2pdb, tleap_dump)
+        return cls._load(mol, pH, prep2pdb, tleap_dump)
     
     @classmethod    
-    def loadFromFile(cls, mol_name, pH=7, prep2pdb={}, tleap_dump=False):
-        shell.cmd.checkFile(f'{mol_name}.sdf')
+    def loadFromFile(cls, file, pH=7, prep2pdb={}, tleap_dump=False):
+        shell.checkFile(f'{file}.sdf')
         
-        mol = PandasTools.LoadSDF(f'{mol_name}.sdf')
+        mol = shell.read(file)
         
-        print(f"**Creating ligand {mol_name} from SDF file**")
+        print(f"Creating non standard ligand from {file}..")
         
-        return cls._load(mol, pH, mol_name, prep2pdb, tleap_dump)
+        return cls._load(mol, pH, prep2pdb, tleap_dump)
     
     @staticmethod    
-    def runG09FromSDF(server, mol, nc, pH=7, parallel=False):
+    def runGaussFromSDF(mol, nc, pH=7, parallel=False):
         
-        name = mol.loc[0]['ChemCompId']
+        print(f"Running Gaussian Calculation on ligand..")
         
-        print(f"**Running Gaussian Optimization for {name} on SDF Dataframe**")
+        print(f"Converting to PDB..")
         
-        pdb, chains = _addH.do(mol, pH, name)
+        pdb, chains, name, elems = _addH.do(mol, pH)
         
-        with open(f'{name}.pdb', 'w') as f: f.write(pdb)
+        shell.write(pdb, f(name,'.pdb'))
         
         print("Generating Gaussian input file using AmberTools Antechamber..")
         
-        shell.cmd.run(f"antechamber -i {name}.pdb -fi pdb -o {name}.com -fo gcrt -nc {nc}")
+        shell.run(f("antechamber -i ",name,".pdb -fi pdb -o ",name,".com -fo gcrt -nc ",nc))
         
-        print(f'Running Gaussian on {server.name}..')
+        shell.query_rate = 30
+        shell.redirectStdout(f(name,'.out')) # add this to local shell
         
-        shell.cmd.query_rate = 30
-        shell.cmd.redirectStdout(f'{name}.out') # add this to local shell
+        if gauss is None:
+            raise Exception("No Gaussian executable given!")
         
-        out = shell.cmd.run(f'g09 {name}.com {name}.out')
-        
-        print("**Done**")
+        out = shell.cmd.run(f(gauss,' ',name,'.com ',name,'.out'))
         
         return out
     
     @staticmethod
-    def getPrepfromG09(mol):
-        print(f"**Converting Gaussian output file to Amber prep**")
-        shell.cmd.checkFile(f"{mol}.out")
+    def getPrepfromGauss(mol):
+        print(f"Converting Gaussian output file to Amber prep..")
+        shell.cmd.checkFile(f(mol,".out"))
         print('Running AmberTools antechamber..')
         out = shell.cmd.run(f"antechamber -i {mol}.out -fi gout -o {mol}.prep -fo prepi -c resp -rn {mol}")
         print(f"Antechamber output dumped...\nResidue {mol} created in {mol}.prep..\n**Done**")
         
         return out
         
-class StdResidue(Ligand):
+class StdLigand(NonStdLigand):
     @classmethod
     def loadFromDF(cls, mol):
-        mol_name = mol.loc[0]['ChemCompId']
+        print(f"Creating standard ligand from SDF data..")
         
-        print(f"**Creating standard residue {mol_name} from SDF Dataframe*")
-        
-        pdb, chains = _addH._donoH(mol, mol_name)
-        return cls(pdb, "", "", 1, mol_name)
+        pdb, chains, mol_name, elems = _addH.donoH(mol)
+        return cls(pdb, "", "", 1, mol_name, mol_name, elems)
     
     @classmethod 
-    def loadFromFile(cls, mol_name):
-        shell.cmd.checkFile(f'{mol_name}.sdf')
-        mol = PandasTools.LoadSDF(f'{mol_name}.sdf')
+    def loadFromFile(cls, file):
+        shell.checkFile(f'{file}.sdf')
         
-        print(f"**Creating standard residue {mol_name} from SDF file**")
+        mol = shell.read(file)
         
-        pdb, chains = _addH._donoH(mol, mol_name)
+        print(f"Creating standard ligand from {file}..")
         
-        return cls(pdb, "", "", 1, mol_name)
+        pdb, chains, mol_name, elems = _addH.donoH(mol)
+        return cls(pdb, "", "", 1, mol_name, mol_name, elems)
     
