e9cdee159fba9080bcaaf2c58f1623672bf64e82
bharurn <bharath.rn@gmail.com>
2020-04-30 22:35:14 +0200

    Added loader, decorator changes

--- analysis/dashboard.py
+++ analysis/dashboard.py
@@ -1,78 +1,108 @@
 from itertools import cycle
 import random
+import threading
+import sys
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+from IPython.display import display
+from ipywidgets import interactive, Layout, HBox, VBox
 
-try:
-    import matplotlib.pyplot as plt
-    import ipywidgets as widgets
-    from IPython.display import display
-    from ipywidgets import interactive, Layout, HBox, VBox
-except ImportError:
-    raise Exception("Please install the analysis verions of the package")
-            
+#decorator PlotBoxDF
+class _PlotBoxDF(object):
+    def __init__(self, foo, x_axis=['Step', 'Time']):
+        self.foo = foo
+        self.x_axis = x_axis
+        self.estimated_time = 0 # not loading bar
+        self.tstep = 0.2
+    
+    def pbar_wrapper(self, function, args=[], kwargs={}):
+        ret = [None]  # Mutable var so the function can store its return value
+        def myrunner(function, ret, *args, **kwargs):
+            ret[0] = function(*args, **kwargs)
 
-#decorator syntax:
-#@plotBoxGen(title of tab, args to be passed to func..)
-def plotBoxGen(*args):
-    def decorator(function):
-        def wrapper(flag, *args1):
-            if flag:
-                a = list(args)
-                return (a[0], a[1:])
-            else: return function(*args1)
+        thread = threading.Thread(target=myrunner, args=(function, ret) + tuple(args), kwargs=kwargs)
+        pbar = tqdm(total=self.estimated_time, file=sys.stdout,\
+                bar_format='{desc}:{bar}', desc='Calculating', leave=False, disable=False)
+
+        thread.start()
+        while thread.is_alive():
+            thread.join(timeout=self.tstep)
+            pbar.update(self.tstep)
+    
+        pbar.close()
+    
+        return ret[0]
+
+    def __call__(self, *args, **kwargs):
+        if self.estimated_time != 0:
+            return self.pbar_wrapper(self.foo, args, kwargs)
+        else:
+            print(args)
+            return self.foo(*args, **kwargs)
+    
+# wrap _PlotBoxDF to allow for deferred calling
+def PlotBoxDF(function=None, x_axis=['Time', 'Step']):
+    if function:
+        return _PlotBoxDF(function)
+    else:
+        def wrapper(function):
+            return _PlotBoxDF(function, x_axis=x_axis)
+
         return wrapper
-    return decorator
-
+            
 class PlotBox:
     def __init__(self, df_gen, *args):
-        self.line1 = None
-        self.ax = None
-        self.fig = None
-        self.df_gen = df_gen
-        self.df_args = args
-        self.df = None
-        self.df_no_ndx = None
-        self.title, self.index = self.df_gen(True)
-        self.xdef_opt = "--Select to Calculate--"
-        self.ydef_opt = "--Not Calculated--"
-       
-    def plot(self, x, y):
-        if self.df is None:
-            self.df = self.df_gen(False, *self.df_args)
-            self.df_no_ndx = self.df.reset_index()
-            self.fixColor()
-            self.fig = plt.figure()
+        self._df_gen = df_gen
+        self._df_args = args
+        self._df = None
+        self._df_no_ndx = None
+        self.title = 'Plot'
+        self._index = self._df_gen.x_axis
+        self._xdef_opt = "--Select to Calculate--"
+        self._ydef_opt = "--Not Calculated--"
+        self.figsize = (7,5.5)
+        self.ax_kwargs = {'marker':'o', 'linewidth':2, 'markersize':4.5, 'linestyle':'-.'}
+    
+    def setAxisProps(self, **kwargs): self.ax_kwargs.update(kwargs)
+        
+    def _plot(self, x, y):
+        if self._df is None:
+            self._df = self._df_gen(*self._df_args)
+            self._df_no_ndx = self._df.reset_index()
+            self._fixColor()
             
-        if y == self.ydef_opt:
-            self.y.options = self.df.columns
+        if y == self._ydef_opt:
+            self._y.options = self._df.columns
             val = x
-            self.x.options = self.index
-            self.x.value = val
+            self._x.options = self._index
+            self._x.value = val
             return
             
-        if x == self.xdef_opt or y == self.ydef_opt: return
+        if x == self._xdef_opt or y == self._ydef_opt: return
         
         else:   
             plt.close()
-            fig = plt.figure(num=f"{y} vs {x} | Use Controls Above to Change Axis & Below to Interact", figsize=(4.5,3))
+            fig = plt.figure(num=f"{y} vs {x}", figsize=self.figsize)
             ax = fig.add_subplot(111)
-            ax.plot(self.df_no_ndx[x], self.df[y], color=self.colors[self.df.columns.get_loc(y)],\
-                             marker='o', linewidth=2.5, markersize=8, linestyle='-.')
+            ax.plot(self._df_no_ndx[x], self._df[y], color=self._colors[self._df.columns.get_loc(y)], **self.ax_kwargs)
             ax.set_autoscaley_on(True)
             ax.set_ylabel(y)
             ax.set_xlabel(x)
+            fig.tight_layout()
             plt.show()
        
-    def fixColor(self):
+    def _fixColor(self):
         c_list = ["r","b","g","c","m","y","k"]
-        if len(c_list) > len(self.df.columns):
-            self.colors = random.sample(["r","b","g","c","m","y","k"], len(self.df.columns))
+        if len(c_list) > len(self._df.columns):
+            self._colors = random.sample(["r","b","g","c","m","y","k"], len(self._df.columns))
         else:
-            self.colors = []
+            self._colors = []
             for i,a in enumerate(cycle(c_list)):
-                if i >= len(self.df.columns): break
-                else: self.colors.append(a)
+                if i >= len(self._df.columns): break
+                else: self._colors.append(a)
         
-    def getDropDown(self, labels, val, desc):
+    def _getDropDown(self, labels, val, desc):
         labels.append(val)
         
         return widgets.Dropdown(
@@ -83,28 +113,27 @@ class PlotBox:
         
     def getBox(self):
         
-        self.box = VBox(children = self.getWidget().children)
-        self.box.layout = Layout(margin='23px 0px 0px 100px')
-        self.box.children[0].layout = Layout(left='0px', top="0px")
-        self.box.children[1].layout = Layout(left='350px', top="-32px")
+        self._box = VBox(children = self.getWidget().children)
+        self._box.layout = Layout(margin='23px 0px 0px 40px')
+        self._box.children[0].layout = Layout(left='20px', top="0px")
+        self._box.children[1].layout = Layout(left='450px', top="-32px")
         
-        return self.box
+        return self._box
     
     def getHBox(self):
         
-        self.box = HBox(children = self.getWidget().children)
+        self._box = HBox(children = self.getWidget().children)
         
-        return self.box
+        return self._box
     
     def getWidget(self):
-        self.x = self.getDropDown(self.index.copy(), self.xdef_opt, 'X Axis:')
-        self.y = self.getDropDown([], self.ydef_opt, 'Y Axis:')
-        return interactive(lambda x, y: self.plot(x, y), x = self.x, y = self.y)
+        self._x = self._getDropDown(self._index.copy(), self._xdef_opt, 'X Axis:')
+        self._y = self._getDropDown([], self._ydef_opt, 'Y Axis:')
+        return interactive(lambda x, y: self._plot(x, y), x = self._x, y = self._y)
     
 def show(*args, **font):
     fnt = {'family' : 'Arial',
-        'weight' : 'bold',
-        'size'   : 20}
+        'size'   : 14}
     
     if font is not None: fnt.update(font)
     
