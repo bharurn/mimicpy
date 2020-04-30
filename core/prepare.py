4597f2657642a097a74e32d3079149ba553cf7f1
bharurn <bharath.rn@gmail.com>
2020-04-30 22:38:34 +0200

    _global changes

--- core/prepare.py
+++ core/prepare.py
@@ -1,7 +1,7 @@
 from ..utils import handlePDB as hpdb
 from ..utils.scripts import mdp
 from .base import Run
-from .._global import host
+import mimicpy._global as _global
 from . import _qmhelper
 from ._constants import bohr_rad
 from ..utils.scripts import cpmd
@@ -14,28 +14,29 @@ class MM(Run):
         self._ion_kwargs = {'pname': 'NA', 'nname': 'CL', 'neutral': ''}
         self._box_kwargs = {'c': '', 'd': 1.9, 'bt': 'cubic'}
         self._solavte_kwargs = {'cs': 'spc216.gro'}
+        super().__init__()
         
     def prepareTopology(self, histidine=[]):
-        print('**Preparing protein topology**')
+        print('Preparing protein topology..')
         
         print(f"Writing {self.protein.name} pdb to confin.pdb..")
-        host.write(self.protein.pdb+self.protein.water, "confin.pdb")
+        _global.host.write(self.protein.pdb+self.protein.water, "confin.pdb")
         
         self.setcurrent('coords', "confin.pdb")
         
         if histidine == []:
             print("Letting Gromacs calculate histidine protonantion states..")
-            self.gmx('pdb2gmx -f confin.pdb -o conf.pdb -water tip3p -ff amber99sb-ildn')
+            Run.gmx('pdb2gmx -f confin.pdb -o conf.pdb -water tip3p -ff amber99sb-ildn')
         else:
             print("Reading histidine protonation states from list..")
             his_str = '\n'.join(histidine)
             his_str = his_str.replace('D1', '0')
             his_str = his_str.replace('E2', '1')
-            self.gmx('pdb2gmx', f = 'confin.pdb', o = 'conf.pdb', water = 'tip3p', ff = 'amber99sb-ildn', his='', stdin=his_str)
+            Run.gmx('pdb2gmx', f = 'confin.pdb', o = 'conf.pdb', water = 'tip3p', ff = 'amber99sb-ildn', his='', stdin=his_str)
         
         self.setcurrent('coords', "conf.pdb")
         
-        pdb = host.read('conf.pdb')
+        pdb = _global.host.read('conf.pdb')
         
         lines = []
         splt = pdb.splitlines()
@@ -51,7 +52,7 @@ class MM(Run):
         print("Adding non-standard residues to structure..")
         conf_pdb = '\n'.join(splt[:len(splt)-i]) + '\n' + self.protein.ligand_pdb + '\n'.join(lines[::-1])
         
-        host.cmd.write(conf_pdb, "conf.pdb")
+        _global.host.write(conf_pdb, "conf.pdb")
         
         self.setcurrent('coords', "conf.pdb")
         
@@ -60,55 +61,57 @@ class MM(Run):
                     "\n\n[ atomtypes ]\n")
         top2 = ''
         
-        print("Combining lignads topology into single file ligands.itp..")
+        print("Combining ligands topology into single file ligands.itp..")
         for ligname, lig in self.protein.ligands.items():
             t1, t2 = lig.splitItp()
 	
             top1 += t1
             top2 += t2 
             
-            print(f"Writing position restraing file for {lig.name}")
+            print(f"Writing position restraint file for {lig.name}")
             
-            host.write(lig.posre, f"posre_{lig.name}.itp")
+            _global.host.write(lig.posre, f"posre_{lig.name}.itp")
             
-            host.cmd.run(f'echo {lig.name} {lig.chains} >> topol.top')
+            _global.host.run(f'echo {lig.name} {lig.chains} >> topol.top')
         
-        host.write(top1+'\n'+top2, f"ligands.itp")
+        _global.host.write(top1+'\n'+top2, f"ligands.itp")
         
-        host.cmd.run(r'sed -i -r "/^#include \".+.ff\/forcefield.itp\"/a #include \"ligands.itp\"" topol.top')
-        host.cmd.run('grep -v SOL topol.top > topol_.top && mv topol_.top topol.top')
-        host.cmd.run(f'echo SOL {self.protein.hoh_mols} >> topol.top')
+        _global.host.run(r'sed -i -r "/^#include \".+.ff\/forcefield.itp\"/a #include \"ligands.itp\"" topol.top')
+        _global.host.run('grep -v SOL topol.top > topol_.top && mv topol_.top topol.top')
+        _global.host.run(f'echo SOL {self.protein.hoh_mols} >> topol.top')
         print("ligands.itp added to topol.top")
         
         self.setcurrent('topology', 'topol.top')
+        print('Topology prepared..')
         
-        print('**Done**')
+        self.saveToYaml()
         
     def setIons(self, **kwargs): self._ion_kwargs = kwargs
     def setBox(self, **kwargs): self._box_kwargs = kwargs
     def setSolvation(self, **kwargs): self._solavte_kwargs = kwargs
     
     def prepareBox(self, genion_mdp = mdp.MDP.defaultGenion()):
-        print("**Preparing system for minimization**")
+        print("Preparing system box..")
         print("Solavting protein..")
         
-        self.gmx('editconf', f = self.getcurrent('coords'), o = 'conf1.gro', **self._box_kwargs)
+        Run.gmx('editconf', f = self.getcurrent('coords'), o = 'conf1.gro', **self._box_kwargs)
         self.setcurrent('coords', 'conf1.gro')
         
-        self.gmx('solvate', cp = 'conf1.gro',o = 'conf2.gro', p = 'topol.top', **self._solavte_kwargs)
+        Run.gmx('solvate', cp = 'conf1.gro',o = 'conf2.gro', p = 'topol.top', **self._solavte_kwargs)
         self.setcurrent('coords', 'conf2.gro')
         
-        host.write(str(genion_mdp), "ions.mdp")
+        _global.host.write(str(genion_mdp), "ions.mdp")
         
         print("Adding ions to neutralize charge..")
-        self.gmx('grompp', f = 'ions.mdp', c = 'conf2.gro', p = 'topol.top', o = 'ions.tpr')
+        Run.gmx('grompp', f = 'ions.mdp', c = 'conf2.gro', p = 'topol.top', o = 'ions.tpr')
         
-        self.gmx('genion', s = 'ions.tpr', o = 'conf3.gro', p = 'topol.top', **self._ion_kwargs, stdin="SOL")
+        Run.gmx('genion', s = 'ions.tpr', o = 'conf3.gro', p = 'topol.top', **self._ion_kwargs, stdin="SOL")
         
         self.setcurrent('coords', 'conf3.gro')
         self.setcurrent('tpr', 'ions.tpr')
+        print('Simulation box prepared..')
         
-        print("**Done**")
+        self.saveToYaml()
         
 class QM(Run):
     
@@ -129,8 +132,8 @@ class QM(Run):
                 slst = ", ".join(lst)
                 raise Exception(f'{k} not a valid selection. Please use the following keywords:\n{slst}')
         
-        pdb_coords = host.read(self.gethistory('coords')[1]) #pdb file after pdb2gmx, with ligands included
-        gro_coords = host.read(self.getcurrent('coords'))
+        pdb_coords = _global.host.read(self.gethistory('coords')[1]) #pdb file after pdb2gmx, with ligands included
+        gro_coords = _global.host.read(self.getcurrent('coords'))
         
         pdb_splt = pdb_coords.splitlines()
         gro_splt = gro_coords.splitlines()
@@ -163,17 +166,17 @@ class QM(Run):
         mdp.dt = 0.002 #dummy value
         mdp.QMMM_grps = 'QMatoms'
         
-        host.cmd.write(str(mdp), 'mimic.mdp')
-        host.cmd.write(_qmhelper.index(self.qmatoms.keys()), 'index.ndx')
+        _global.host.write(str(mdp), 'mimic.mdp')
+        _global.host.write(_qmhelper.index(self.qmatoms.keys()), 'index.ndx')
         
-        self.gmx('grompp', f='mimic.mdp', c=self.getcurrent('coords'),\
+        Run.gmx('grompp', f='mimic.mdp', c=self.getcurrent('coords'),\
                  p=self.getcurrent('topology'), o='mimic.tpr', pp='processed.top', n='index.ndx')
         self.setcurrent('tpr', 'mimic.tpr')
         
-        qmatoms_updated = _qmhelper.pptop(self._qmatoms, self._protein_res, host.cmd.read('processed.top'))
+        qmatoms_updated = _qmhelper.pptop(self._qmatoms, self._protein_res, _global.host.read('processed.top'))
         
         inp.mimic = cpmd.Section()
-        inp.mimic.paths = f"1\n{host.cmd.pwd()}/gmx"
+        inp.mimic.paths = f"1\n{_global.host.pwd()}/gmx"
         inp.mimic.box = '  '.join(self._mm_box)
         
         inp = _qmhelper.getOverlaps_Atoms(qmatoms_updated, inp)
