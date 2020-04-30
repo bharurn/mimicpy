1de91c09c1854ca19fa4cbfb01a55143ae80bb03
bharurn <bharath.rn@gmail.com>
2020-04-30 22:38:50 +0200

    _global changes

--- system/_getItp.py
+++ system/_getItp.py
@@ -1,7 +1,7 @@
-from .._global import host as shell, parmchk, tleap, acpype
+import mimicpy._global as _global
 
 def _cleanprep(mol, prep_to_pdb):
-    prep = shell.cmd.read(f'{mol}.prep')
+    prep = _global.host.read(f'{mol}.prep')
     
     for i in prep_to_pdb:
         prep = prep.replace(i, prep_to_pdb[i])
@@ -64,12 +64,12 @@ def do(mol, conv, tleap_dump=False):
     
     prep = f"{mol}.prep"
     
-    shell.cmd.checkFile(prep)
+    _global.host.checkFile(prep)
     
     if conv != {}:
         print(f"Mapping prep of {mol}.prep atom names to that of pdb using dictionary provided..")
         
-        shell.cmd.write(_cleanprep(mol, conv), "params.prep")
+        _global.host.write(_cleanprep(mol, conv), "params.prep")
         
         print("Ouput saved to params.prep")
         
@@ -77,7 +77,7 @@ def do(mol, conv, tleap_dump=False):
         
     print(f"Running AmberTools parmchk on {prep}..")
     
-    shell.cmd.run(f'{parmchk} -i {prep} -f prepi -o params.frcmod')
+    _global.host.run(f'{_global.parmchk} -i {prep} -f prepi -o params.frcmod')
     
     print("Output saved to params.frcmod..")
     
@@ -85,13 +85,13 @@ def do(mol, conv, tleap_dump=False):
     tleap_in += f"loadamberprep {prep}\n"
     tleap_in += f"loadamberparams params.frcmod\n"
     
-    if shell.cmd.checkFile(f"{mol}.frcmod", throw=False):
+    if _global.host.checkFile(f"{mol}.frcmod", throw=False):
         tleap_in += f"loadamberparams {mol}.frcmod\n"
     
     tleap_in += f"saveamberparm {mol} {mol}.prmtop {mol}.inpcrd\nquit"
     
     print("Running AmberTools LEaP..")
-    output = shell.cmd.run(f'{tleap} -f -', stdin=tleap_in)
+    output = _global.host.run(f'{_global.tleap} -f -', stdin=tleap_in)
     
     if tleap_dump:
         print("Dumping LEaP input script used:\n\n")
@@ -99,21 +99,21 @@ def do(mol, conv, tleap_dump=False):
         print("Dumping LEaP output:\n\n")
         print(output)
     
-    if shell.cmd.fileExists(f'{mol}.prmtop') == False or shell.cmd.fileExists(f'{mol}.inpcrd') == False:
+    if _global.host.fileExists(f'{mol}.prmtop') == False or _global.host.fileExists(f'{mol}.inpcrd') == False:
         raise Exception(f'LEaP error!\n{output}')
     
     print(f"Output saved to {mol}.prmtop and {mol}.inpcrd..")
     
     print("Converting to Gromacs topology using Acpype..")
-    output = shell.cmd.run(f'{acpype} -p {mol}.prmtop -x {mol}.inpcrd')
+    output = _global.host.run(f'{_global.acpype} -p {mol}.prmtop -x {mol}.inpcrd')
     
-    if shell.cmd.fileExists(f'{mol}_GMX.gro') == False or shell.cmd.fileExists(f'{mol}_GMX.top') == False:
+    if _global.host.fileExists(f'{mol}_GMX.gro') == False or _global.host.fileExists(f'{mol}_GMX.top') == False:
         raise Exception(f'Acpype error!\n{output}')
         
     print(f"Output saved to {mol}_GMX.gro and {mol}_GMX.top..")
     
     print("Cleaning Acpype output..")
-    itp = shell.cmd.read(f'{mol}_GMX.top')
+    itp = _global.host.read(f'{mol}_GMX.top')
     
     print("Renaming atoms to avoid conflict..")
     itp, hvy = _cleanItp(itp, mol) # rename atoms, remove uneeded sections, get list of heavy atoms for posre
