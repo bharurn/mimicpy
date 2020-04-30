2b028a99accb1e996b6d1193fe2c9f90046162ff
bharurn <bharath.rn@gmail.com>
2020-04-30 22:37:36 +0200

    mdrun changes

--- core/md.py
+++ core/md.py
@@ -1,38 +1,35 @@
 from ..utils.scripts import mdp
 from .base import Run
-from .._global import host
+import mimicpy._global as _global
 
 class MD(Run):
     
-    def __init__(self, **kwargs):
-        
-        self.jobscript = None
-        
-        for k,v in kwargs.items():
-            self.setcurrent(k, v)
-    
     def SlurmSettings(self, settings):
-        print(f"Setting SLURM job settings from jobscript {settings.name}..")
+        print(f"Setting Slurm job settings from jobscript {settings.name}..")
         self.jobscript = settings
-        print(f"Transferring sources, modules and other header commands from Host to job script..")
-        self.jobscript.module(host.cmd.modules)
-        self.jobscript.source(host.cmd.sources)
-        self.jobscript.addMany(host.cmd.loaders)
+        print(f"Transferring sources, modules and other header commands from _global.host to job script..")
+        self.jobscript.module(_global.host.modules)
+        self.jobscript.source(_global.host.sources)
+        self.jobscript.addMany(_global.host.loaders)
         
         return self.jobscript
             
     def mdrun(self, new, **kwargs):
         
-        def _do(self, new, **kwargs):
-            if self.jobscript in None:
-                self.gmx('mdrun', **kwargs, deffnm = new)
+        def _do(new, **kwargs):
+            if self.jobscript is None:
+                 # shell.local -- add polling for stdout and print timestep
+                _global.host.redirectStdout(f"{new}.log")
+                Run.gmx('mdrun', **kwargs, deffnm = new)
+                print("Done..")
             else:
-                self.jobscript.add(self.gmx('mdrun', **kwargs, deffnm = new, onlycmd=True))
+                return self.jobscript.add(Run.gmx('mdrun', **kwargs, deffnm = new, onlycmd=True, nonverbose=True))
         
-        if kwargs['noappend'] == True:
-            out = self._do(new, **kwargs, noappend = '')
+        if 'noappend' in kwargs:
+            if kwargs['noappend'] == True: out = self._do(new, **kwargs, noappend = '')
+            else: out = _do(new, **kwargs)
         else:
-            out = self._do(new, **kwargs)
+            out = _do(new, **kwargs)
             
         self.setcurrent('edr', f"{new}.edr")
         self.setcurrent('trr', f"{new}.trr")
@@ -40,11 +37,11 @@ class MD(Run):
         self.setcurrent('log', f"{new}.log")
         
         if out == None:
-            print("MDrun job submmitted as a SLURM job script"
-                 f"{self.jobscript.name}.sh..\nPlease use squeue() for details and check for job ID {out}..")
-            return host.cmd.sbatch(self.jobscript)
+            print("MDrun job submmitted as a Slurm job script"
+                 f"{self.jobscript.name}.sh with the job ID {out}.."
+                 f"\nThe host and/or this script can be safely closed..")
+            return _global.host.sbatch(self.jobscript)
         else:
-            print("Running mdrun on the login node..\nConsider using SLURM if this is a production run..")
             return None
     
     def continueRun(self, new, until=0, extend=0, fromcrash=False, noappend=True):
@@ -53,12 +50,12 @@ class MD(Run):
             return self.mdrun(new, s = self.getcurrent('tpr'), cpi = self.getcurrent('cpt'), noappend = noappend)
         elif until != 0:
             
-            self.gmx('convert-tpr', s = self.getcurrent('tpr'), until = until, o = f'{new}.tpr')
+            Run.gmx('convert-tpr', s = self.getcurrent('tpr'), until = until, o = f'{new}.tpr')
             self.setcurrent('tpr', f"{new}.tpr")
             return self.mdrun(s = f'{new}.tpr', cpi = self.getcurrent('cpt'), deffnm = new, noappend = noappend)
                 
         elif extend:
-            self.gmx('convert-tpr', s = self.getcurrent('tpr'), extend = extend, o = f'{new}.tpr')
+            Run.gmx('convert-tpr', s = self.getcurrent('tpr'), extend = extend, o = f'{new}.tpr')
             self.setcurrent('tpr', f"{new}.tpr")
             return self.mdrun(s = f'{new}.tpr', cpi = '{cpt}.cpt', deffnm = new, noappend = noappend)
     
@@ -66,25 +63,23 @@ class MD(Run):
         
         if 'disp' not in kwargs: disp = new.title()
         else:
-            disp = kwargs['name']
-            del kwargs['name']
+            disp = kwargs['disp']
+            del kwargs['disp']
         
-        print(f"**Starting classical MD calculation: {disp}**")
+        print(f"Starting classical MD calculation: {disp}..")
         
         print(f"All files will be saved with the name {new}..")
         
-        self.write(str(mdp), f"{new}.mdp")
+        _global.host.write(str(mdp), f"{new}.mdp")
         
-        print("Running grompp..")
-        
-        self.gmx('grompp', f = f'{new}.mdp', c = self.getcurrent('coords'),\
+        Run.gmx('grompp', f = f'{new}.mdp', c = self.getcurrent('coords'),\
                      p = self.getcurrent('topology'), o = f"{new}.tpr", **kwargs)
         
         self.setcurrent('tpr', f"{new}.tpr")
         
         out = self.mdrun(new)
         
-        print("**Done**")
+        print("Done..")
         
         return out
         
