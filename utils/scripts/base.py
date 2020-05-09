f965f7b85d3841f1550fadc46af6a78fa4c86e34
bharurn <bharath.rn@gmail.com>
2020-05-09 20:45:01 +0200

    Base class for scripts

---
+++ utils/scripts/base.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict 
+
+class Script(object):
+    def __init__(self, **kwargs):
+        super().__setattr__('__orddict__', OrderedDict())
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            
+    def __setattr__(self, key, value):
+        if key.startswith('_') or key == 'hasparams' or key == 'params':
+            self.__dict__[key] = value
+        else:
+            self.__orddict__[key] = value
+        
+    def __getattr__(self, key):
+        if key.startswith('_') or key == 'hasparams' or key == 'params':
+            return self.__getattribute__('__dict__')[key]
+        else:
+            return self.__getattribute__('__orddict__')[key]
+    
+    def hasparam(self, key):
+        if key in self.__orddict__:
+            return True
+        else:
+            return False
+    
+    def params(self): return self.__orddict__
