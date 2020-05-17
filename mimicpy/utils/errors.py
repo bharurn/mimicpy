7f5092ca3dd3fb69f994808902589b1c96428816
bharurn <bharath.rn@gmail.com>
2020-05-17 23:22:23 +0200

    Edited error classes

--- mimicpy/utils/errors.py
+++ mimicpy/utils/errors.py
@@ -1,27 +1,39 @@
+from .._global import _Global
+
 class MiMiCPyError(Exception):
    """Base class for other exceptions"""
-   pass
+   def __init__(self, msg):
+       _Global.logger.close()
 
-class RemoteShellError(MiMiCPyError):
+class ExecutionError(MiMiCPyError):
+    
+    def __init__(self, cmd, msg):
+        super().__init__(msg)
+        self.cmd = cmd
+        self.msg = msg
+        
+    def __str__(self):
+        return f"Command attempted {self.cmd}\nself.msg"
+
+class GromacsError(ExecutionError):
     pass
 
-class OpenBabelError(MiMiCPyError):
-    pass
-
-class LEaPError(MiMiCPyError):
-    pass
-
-class AcpypeError(MiMiCPyError):
-    pass
-
-class GromacsError(MiMiCPyError):
-    pass
-
-class SlurmError(MiMiCPyError):
-    pass
+class SlurmBatchError(ExecutionError):
+    def __str__(self):
+        return f"Attempted to run sbatch {self.cmd}\nself.msg"
 
 class EnvNotSetError(MiMiCPyError):
-    pass
+    def __init__(self, cmd, msg):
+        super().__init__(msg)
+        self.msg = msg
+        
+    def __str__(self):
+        return f"{self.cmd} not set! Please set it in host.{self.msg}"
 
 class ScriptError(MiMiCPyError):
-    pass
+     def __init__(self, cmd, msg):
+        super().__init__(msg)
+        self.msg = msg
+        
+     def __str__(self):
+        return f"{self.msg} not a paramater of the script"
