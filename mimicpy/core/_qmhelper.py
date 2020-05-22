dd53d8d21698cbabc090a65fba00d061cd7e7dbb
bharurn <bharath.rn@gmail.com>
2020-05-22 20:19:43 +0200

    Cleaned up funcs

--- mimicpy/core/_qmhelper.py
+++ mimicpy/core/_qmhelper.py
@@ -7,56 +7,17 @@ pptop() and getOverlaps_Atoms() adapted from the prepare_qmmm python script by V
 
 """
 
-from ..utils.constants import  element_names, bohr_rad
+from ..parsers.mpt import Reader as MPTReader
+from ..utils.constants import bohr_rad
 from ..scripts.cpmd import Atom
 from collections import OrderedDict 
 from ..scripts import cpmd
-from .._global import _Global as gbl
 
-def parse_selec(selection, df):
-    """Translate selection language string into pandas dataframe selection"""
-    
-    # if selection is a lambda func, then just call and return it
-    # this is provided for debuggin puposes
-    LAMBDA = lambda:0
-    if isinstance(selection, type(LAMBDA)):
-        return df[selection(df)]
-            
-    # below code translates selection langauge to pandas boolean
-    # selection eg., resName is SER and number < 25 and chainID not B
-    # will be translated to df['resName'] == 'SER' and df.index == 25 and df['chainID'] != 'B'
-    
-    ev = '' # cint onverted string
-    i = 0 # counter to keep track of word position
-    for s in selection.split():
-        if i == 0: # if starting of set
-            ev += f"(df['{s}']"
-        # if and/or encountered, reset i to -1 (will become 0 due to i+= 1 at end)
-        # so we can start parsing again
-        elif s == 'or':
-            ev += f' | '
-            i = -1
-        elif s == 'and':
-            ev += f' & '
-            i = -1
-        elif s == 'is':
-            ev += '=='
-        elif s == 'not':
-            ev += '!='
-        elif s == '>' or s == '>=' or s == '<' or s == '<=':
-            ev += s
-        else: # parse everything else, meant for the third word
-            if s.isnumeric():
-                ev += f"{s})"
-            else:
-                ev += f"'{s}')"
-                
-        i += 1
-
-    ev = f"df.loc[{ev}]" # eg., df.loc[ df['resName'] == 'SER' and df.index == 25 and df['chainID'] != 'B' ]
-    ev = ev.replace("df['number']","df.index") # replace df['number'] to df.index as number is the index of df
-    gbl.logger.write('debug2', f'Selection command translated to: {ev}')
-    return eval(ev) # evaluate string and return the dataframe
+def _cleanqdf(qdf):
+    columns = MPTReader.columns       
+    columns.extend(['x','y','z'])
+    lst = [l for l in qdf.columns if l not in columns]
+    return qdf.drop(lst, axis=1)
 
 def index(qmids, name):
     """Write list of atoms to index, and returns as sting"""
@@ -65,56 +26,6 @@ def index(qmids, name):
         index += f'{i} '
         
     return index
-    
-def pptop(unk_lst, all_names, top):
-    """
-    Parse prerocessed topology and return mapping of 
-    atom_name->element symbol & atom_name->parital charge
-    
-    Logic:
-        Read first [ atomtypes ] section and get mapping from atom types->element symbol
-        All other [ atomtypes ] are not read as these correspond to non std ligands and we already have the symbol info
-        for those from system.ligands.NonStdLigands
-        Read all [ atoms ] sections:
-            Get mapping of atom names->q
-            Get mapping of atom names->element symbol through atom types->element symbol mapping
-    """
-    
-    atm_names_to_symb = dict((el,None) for el in unk_lst) # init atm_name->symb dict with only those that are unknown
-    atm_names_to_q = dict((a,None) for a in all_names) # init atm_name->q dict
-    
-    atm_types_to_symb = {} # init atom types to symbol
-    start1 = False
-    start2 = False
-    
-    for line in top.splitlines():
-        if '[ atomtypes ]' in line and atm_types_to_symb == {}: # read only first atomtypes
-            start1 = True
-        elif start1:
-            if '[' and ']' in line:
-                start1 = False
-            elif not line.startswith(';') and line.strip() != '': # ignore comments and null lines
-                e = line.split()[0] # first val is atom type
-                n = int(line.split()[1])-1 # second is element no.
-                atm_types_to_symb[e]  = element_names[n] # fill up atom types->symbol mapping
-        elif '[ atoms ]' in line: # read all atom sections
-            start2 = True
-        elif start2:
-            if '[' and ']' in line:
-                start2 = False
-            elif not line.startswith(';') and line.strip() != '': # ignore comments and null lines
-                atm_name = line.split()[4] # fifth val is atom name
-                atm_type = line.split()[1] # second val is atom type
-                q = line.split()[6] # seventh val is charge
-                
-                if atm_name in atm_names_to_q.keys():
-                    if atm_names_to_q[atm_name] == None:
-                        atm_names_to_q[atm_name] = float(q) # atom names to charge
-                    
-                    if atm_name in atm_names_to_symb.keys() and atm_names_to_symb[atm_name] == None:
-                        atm_names_to_symb[atm_name] = atm_types_to_symb[atm_type] # atoms names to symbol
-        
-    return atm_names_to_symb, atm_names_to_q
 
 def getOverlaps_Atoms(qmatoms, inp):
     """
@@ -140,7 +51,7 @@ def getOverlaps_Atoms(qmatoms, inp):
     mi = [None, None, None] # min coords
     for i, rows in qmatoms.iterrows():
         elem = rows['element']
-        idx = rows['number']
+        idx = rows['id']
         coords = [rows['x'], rows['y'], rows['z']]
         link = rows['link']
         
