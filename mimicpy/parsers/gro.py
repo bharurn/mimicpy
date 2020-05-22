e731d0b328b3c36c54635c566e7e379702a7c107
bharurn <bharath.rn@gmail.com>
2020-05-22 21:35:49 +0200

    Minor changes

--- mimicpy/parsers/gro.py
+++ mimicpy/parsers/gro.py
@@ -1,6 +1,6 @@
 from .._global import _Global as _global
 
 def getBox(gro):
-    last = _global.host.run(f'tail -n 1 {gro}') # fast access of last line, requires UNIX shell
-    
-    return [float(v) for v in last.split()]
+    # fast access of last line, requires UNIX shell
+    tail = _global.host.run(f'tail -n 1 {gro}')
+    return [float(v) for v in tail.split()]
