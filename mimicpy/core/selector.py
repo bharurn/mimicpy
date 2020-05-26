428521cf7eb58bac69920b62079a0f919965a782
bharurn <bharath.rn@gmail.com>
2020-05-27 01:37:04 +0200

    Added base selector class

--- mimicpy/core/selector.py
+++ mimicpy/core/selector.py
@@ -1,16 +1,69 @@
 from ..shell import _local
 from .._global import _Global as gbl
-from .base import BaseHandle
+from ..parsers import gro
 import xmlrpc.client as xmlrpclib
 import pandas as pd
 
 class Selector:
-    def loadCoords(self, gro):
-        pass
+    def load(self, mpt, gro_file):
+        gro_df, box = gro.read()
+        self.df = mpt.getFull()
+        self.df = self.df.merge(gro_df, left_on='id', right_on='id')
+        return box
+        
     def select(self, selection):
-        pass
+        """Translate selection language string into pandas dataframe selection"""
+        
+        if selection is None:
+            #raise SelectionError
+            pass
+        
+        df = self.df
+        
+        # if selection is a lambda func, then just call and return it
+        # this is provided for debuggin puposes
+        LAMBDA = lambda:0
+        if isinstance(selection, type(LAMBDA)):
+            return df[selection(df)]
+            
+        # below code translates selection langauge to pandas boolean
+        # selection eg., resname is SER and id < 25 and mol not Protein_chain_B
+        # will be translated to df['resName'] == 'SER' and df.index == 25 and df['chainID'] != 'B'
+    
+        ev = '' # converted string
+        i = 0 # counter to keep track of word position
+        for s in selection.split():
+            if i == 0: # if starting of set
+                ev += f"(df['{s}']"
+                # if and/or encountered, reset i to -1 (will become 0 due to i+= 1 at end)
+                # so we can start parsing again
+            elif s == 'or':
+                ev += f' | '
+                i = -1
+            elif s == 'and':
+                ev += f' & '
+                i = -1
+            elif s == 'is':
+                ev += '=='
+            elif s == 'not':
+                ev += '!='
+            elif s == '>' or s == '>=' or s == '<' or s == '<=':
+                ev += s
+            else: # parse everything else, meant for the third word
+                if s.isnumeric():
+                    ev += f"{s})"
+                else:
+                    ev += f"'{s}')"
+                
+            i += 1
 
-class PyMOL(BaseHandle):
+        ev = f"df.loc[{ev}]" # eg., df.loc[ df['resName'] == 'SER' and df.index == 25 and df['chainID'] != 'B' ]
+        ev = ev.replace("df['number']","df.index") # replace df['number'] to df.index as number is the index of df
+        gbl.logger.write('debug2', f'Selection command translated to: {ev}')
+        
+        return eval(ev) # evaluate string and return the dataframe
+
+class PyMOL(Selector):
     
     def _hook(self, out):
         if 'xml-rpc server running on host' in out:
@@ -38,12 +91,15 @@ class PyMOL(BaseHandle):
         self.forcelocal = forcelocal
         self.downloadpath = downloadpath
     
-    def loadCoords(self, gro):
+    def load(self, mpt, gro):
         if not gbl.host.isLocal() and not self.forceLocal:
             gbl.host.sftp.get(gro, self.downloadpath)
             gro = self.downloadpath
         
         self.cmd.load(gro)
+        self.mpt = mpt
+        
+        return gro.getBox(gro)
     
     def select(self, selection=None):
         if selection is None: selection = 'sele'
@@ -58,7 +114,7 @@ class PyMOL(BaseHandle):
         pymol_sele = pymol_sele.rename(columns={"name": "pm_name", "symbol": "pm_symbol", "resn": "pm_resn",\
                                "resi_number": "pm_resi_number"})
     
-        mpt_sele = self.mpt.selectAtoms(pymol_sele['id'])
+        mpt_sele = self.mpt.selectByIDs(pymol_sele['id'])
         
         # TO DO: check if names/resname, etc. are same and issue warnings accordingly
         
