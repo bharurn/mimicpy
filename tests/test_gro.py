a521f2bfbec5672e186cb18b63b0b3ced3941f4b
fschackert <florian.schackert@t-online.de>
2020-07-30 13:38:06 +0200

    Major changes in gro/top/itp handling

--- tests/test_gro.py
+++ tests/test_gro.py
@@ -1,35 +1,45 @@
-from mimicpy.parsers.gro import read
-import time
+from mimicpy.io.gro import Gro
 
-def get_gro_info(gro_file):
+
+def _get_gro_info(gro_file):
+
     with open(gro_file) as f:
         lines = f.readlines()
-        no = int(lines[1].strip())
+        number_of_atoms = int(lines[1].strip())
         box = lines[-1].split()
     
-    return no, box
+    return number_of_atoms, box
 
-def check_gro(file, gro_data, box, cols):
-    gro_data, box = read(file)
-    assert set(gro_data.columns) == cols
-    
-    no, box_ = get_gro_info(file)
-    assert all([a == float(b) for a, b in zip(box, box_)] )
-    assert len(gro_data) == no
 
-def test_gro_read():
-    gro1, box = read('gro_files/gro1.gro')
-    check_gro('gro_files/gro1.gro', gro1, box, {'x', 'y', 'z', 'force-x', 'force-y', 'force-z'})
-    gro2, box = read('gro_files/gro2.gro')
-    check_gro('gro_files/gro2.gro', gro2, box, {'x', 'y', 'z'})
-    
-def test_gro_time():
-    start = time.time()
-    gro1, box = read('gro_files/4aj3.gro')
-    elapsed_time = (time.time() - start) 
-    
-    assert elapsed_time < 1.5
-    
-    check_gro('gro_files/4aj3.gro', gro1, box, {'x', 'y', 'z', 'force-x', 'force-y', 'force-z'})
-    
-    
+def test_read():
+    file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check)
+    coords, box = gro_file.read(buffer=1)
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+    file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check)
+    coords, box = gro_file.read(buffer=2)
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+    file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check)
+    coords, box = gro_file.read()
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+    file_to_check = 'gro_files/4aj3.gro'
+    gro_file = Gro(file_to_check)
+    coords, box = gro_file.read()
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
--- tests/test_topol.py
+++ tests/test_topol.py
@@ -1,5 +1,6 @@
 import mimicpy
 from mimicpy.utils.errors import ParserError
+from mimicpy.io.top import Top
 import pickle
 import pytest
 
@@ -10,7 +11,9 @@ def test_dppc():
     warns = mimicpy.utils.logger.LogString()
     mimicpy.redirectWarnings(warns)
     
-    mol_list, topol_dict = mimicpy.parsers.top.read('dppc/topol.top', guess_elems=False)
+    top = Top('dppc/topol.top')
+    mol_list, topol_dict = top.read()
+ #   mol_list, topol_dict = mimicpy.parsers.top.read('dppc/topol.top', guess_elems=False)
     
     files = ['topol.top', 'gromos53a6.ff/forcefield.itp', 'gromos53a6.ff/ffnonbonded.itp', 'dppc.itp', 'gromos53a6.ff/spc.itp']
     files = ['dppc/'+i for i in files]
@@ -18,7 +21,7 @@ def test_dppc():
     for i in log.splitlines():
         assert i.split()[0] in files
     
-    assert warns == 'Cannot find dppc/posre.itp, skipping..\n'
+ #   assert warns == 'Cannot find dppc/posre.itp, skipping..\n'
     
     assert mol_list==[('DPPC', 2), ('SOL', 100)]
     
@@ -43,6 +46,7 @@ def test_4aj3():
         assert mimicpy.parsers.top.read('4aj3/topol.top', guess_elems=False)
     assert "Cannot determine atomic symbol for atom with name C6N and type NAP_CA in residue NAP" in str(e.value) 
    
-    mol_list, topol_dict = mimicpy.parsers.top.read('4aj3/topol.top', guess_elems=True)
+    top = Top('4aj3/topol.top')
+    mol_list, topol_dict = top.read()
     
     assert mol_list==[('Protein', 1), ('NAP', 1), ('ICT', 1), ('SOL', 47708), ('NA', 18), ('SOL', 18)]
