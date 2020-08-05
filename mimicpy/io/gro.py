c1607dcf2bab4f972d4c7b956af40db3aa2a4f40
bharurn <bharath.rn@gmail.com>
2020-08-05 17:40:30 +0200

    Changed Gro to use Parser

--- mimicpy/io/gro.py
+++ mimicpy/io/gro.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+from .parser import Parser
 
 
 class Gro:
@@ -35,39 +36,40 @@ class Gro:
         def string_to_array(string):
             return np.array(list(map(mapped, string.split())))
 
-        with open(self.file, 'rb') as f:
-            f.readline()
-            number_of_atoms = int(f.readline().decode())
-            first_atom_line = f.readline().decode()
-            buffered_lines = len(first_atom_line) * self.buffer
+        f = Parser(self.file)
+        
+        f.readline()
+        number_of_atoms = int(f.readline())
+        first_atom_line = f.readline()
+        f.buffer = len(first_atom_line) * self.buffer
 
-            number_of_rows = len(first_atom_line.split()) - 3
-            if number_of_rows == 3:
-                cols = ['x', 'y', 'z']
-            elif number_of_rows == 6:
-                cols = ['x', 'y', 'z', 'v_x', 'v_y', 'v_z']
-            else:  # Raise exception
-                pass
+        number_of_rows = len(first_atom_line.split()) - 3
+        
+        if number_of_rows == 3:
+            cols = ['x', 'y', 'z']
+        elif number_of_rows == 6:
+            cols = ['x', 'y', 'z', 'v_x', 'v_y', 'v_z']
+        else:  # Raise exception
+            pass
 
-            values = string_to_array(first_atom_line)
-            i = 0
+        values = string_to_array(first_atom_line)
+        i = 0
 
-            while i < number_of_atoms:
-                string = f.read(buffered_lines).decode()
-                i += self.buffer
-                values = np.append(values, string_to_array(string))
+        for string in f:
+           i += self.buffer
+           values = np.append(values, string_to_array(string))
 
-            values = values[~np.isnan(values)]
+        values = values[~np.isnan(values)]
 
-            expected_len = number_of_atoms * number_of_rows
-            if len(values) == expected_len:
-                coords = values
-                box = f.readline().decode().split()
-            elif len(values) == expected_len+3:
-                coords = values[:-3]
-                box = values[-3:]
-            else:  # Raise exception
-                pass
+        expected_len = number_of_atoms * number_of_rows
+        if len(values) == expected_len:
+            coords = values
+            box = f.readline().decode().split()
+        elif len(values) == expected_len+3:
+            coords = values[:-3]
+            box = values[-3:]
+        else:  # Raise exception
+            pass
 
         coords = pd.DataFrame(coords.reshape(number_of_atoms, number_of_rows), columns=cols)
         coords['id'] = coords.index.to_numpy()+1
--- tests/test_gro.py
+++ tests/test_gro.py
@@ -1,3 +1,4 @@
+import time
 from mimicpy.io.gro import Gro
 
 
@@ -11,35 +12,56 @@ def _get_gro_info(gro_file):
     return number_of_atoms, box
 
 
-def test_read():
+def test_gro1():
     file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check, buffer=1) # reading line by line
+    coords, box = gro_file.coords, gro_file.box
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+    file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check, buffer=2) # reading 2 lines at a time
+    coords, box = gro_file.coords, gro_file.box 
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+    file_to_check = 'gro_files/gro1.gro'
+    gro_file = Gro(file_to_check)
+    coords, box = gro_file.coords, gro_file.box 
+    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
+    box = [str(a) for a in box] # conversion for comparison to naive reader function
+    
+    assert set(coords.columns) == {'x', 'y', 'z', 'v_x', 'v_y', 'v_z'}
+    assert box == box_to_check
+    assert len(coords) == number_of_atoms_to_check
+
+def test_gro2():
+    file_to_check = 'gro_files/gro2.gro'
     gro_file = Gro(file_to_check, buffer=1)
     coords, box = gro_file.coords, gro_file.box
     number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
     box = [str(a) for a in box] # conversion for comparison to naive reader function
+    
+    assert set(coords.columns) == {'x', 'y', 'z'}
     assert box == box_to_check
     assert len(coords) == number_of_atoms_to_check
-
-    file_to_check = 'gro_files/gro1.gro'
-    gro_file = Gro(file_to_check, buffer=2)
-    coords, box = gro_file.coords, gro_file.box 
-    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
-    box = [str(a) for a in box] # conversion for comparison to naive reader function
-    assert box == box_to_check
-    assert len(coords) == number_of_atoms_to_check
-
-    file_to_check = 'gro_files/gro1.gro'
-    gro_file = Gro(file_to_check)
-    coords, box = gro_file.coords, gro_file.box 
-    number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
-    box = [str(a) for a in box] # conversion for comparison to naive reader function
-    assert box == box_to_check
-    assert len(coords) == number_of_atoms_to_check
-
+    
+def test_large_gro():
     file_to_check = 'gro_files/4aj3.gro'
+    
+    start = time.time()
     gro_file = Gro(file_to_check)
+    elapsed_time = (time.time() - start) 
+    assert elapsed_time < 1.5
+    
     coords, box = gro_file.coords, gro_file.box
     number_of_atoms_to_check, box_to_check = _get_gro_info(file_to_check)
     box = [str(a) for a in box] # conversion for comparison to naive reader function
+    
+    assert set(coords.columns) == {'x', 'y', 'z', 'v_x', 'v_y', 'v_z'}
     assert box == box_to_check
     assert len(coords) == number_of_atoms_to_check
