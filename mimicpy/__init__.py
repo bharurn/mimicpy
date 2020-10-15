7130d7cd8c95c75b62096e0d7f803d21f53e3815
fschackert <florian.schackert@t-online.de>
2020-10-15 16:18:23 +0200

    Deleted shell

--- mimicpy/__init__.py
+++ mimicpy/__init__.py
@@ -1,10 +1,7 @@
 import logging
-from .shell import shell
 from ._version import __version__
 from ._authors import __authors__
-from ._global import _Global as gbl
 
-gbl.host = shell.Local('.', None)
 logging.basicConfig(filename='mimicpy.log',  # Maybe move to __main__.py to set basicConfig
                     format='%(asctime)s %(levelname)s %(message)s',
                     filemode='w',
