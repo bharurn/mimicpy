a9838234870d7f9f14da382f2d013f4dd69b1978
bharurn <bharath.rn@gmail.com>
2020-11-04 18:35:52 +0100

    Common table printing

--- mimicpy/utils/strings.py
+++ mimicpy/utils/strings.py
@@ -7,3 +7,15 @@ def clean(txt, comments=None):
         for c in comments:
             txt = re.sub(re.compile(f"{c}(.*)\n" ) ,"\n" , txt)  # Strip comments
     return re.sub(re.compile(r"[\n]+"), "\n", txt)
+
+def print_dict(dct, col1, col2, printer):
+    
+    n1 = len(col1)+1
+    n2 = len(col2)+1
+    
+    printer("+---------------------+")
+    printer("| {:^{n1}}| {:^{n2}}|".format(col1, col2, n1=n1, n2=n2))
+    printer("+---------------------+")
+    for k,v in dct.items():
+        printer("| {:^{n1}}| {:^{n2}}|".format(k, v, n1=n1, n2=n2))
+        printer("+---------------------+")
